9a0ce527596eb4c423a78e7fcfabce19741a9f8c
morti <qstevens@hotmail.be>
2024-04-25 14:47:21 +0200

    [ADD] first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+
+env = gym.make('MsPacman-v4', render_mode="human")
+env.reset()
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
