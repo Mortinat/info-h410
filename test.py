615bf70365bc50d907b392245851769b8e34cfac
morti <qstevens@hotmail.be>
2024-05-18 17:31:27 +0200

    [Upd] test script to benchmark time

--- test.py
+++ test.py
@@ -2,16 +2,19 @@ import minimax as mm
 from common import MINIMAX, EMPTY, ROW_COUNT, COLUMN_COUNT, WINDOW_LENGTH
 import copy
 import math
+import time
 
-
+time_list = []
 with open("Test_L2_R1", "r") as f:
-    print(ROW_COUNT, COLUMN_COUNT)
     empty_board = [[EMPTY for _ in range(ROW_COUNT)] for _ in range(COLUMN_COUNT)]
     for line in f.readlines():
-        print(line)
         board = mm.BoardMinimax(board=copy.deepcopy(empty_board), turn=1, rounds=0)
         split_line = line.split(" ")
         for i in split_line[0]:
             row = board.get_next_open_row(int(i)-1)
             board.drop_piece(row, int(i)-1)
-        print(mm.negamax(board, 10000, -math.inf, math.inf))
+        start = time.time()
+        result = mm.negamax(board, 10000, -math.inf, math.inf)
+        stop = time.time()
+        time_list.append(stop-start)
+        print(sum(time_list)/len(time_list))
